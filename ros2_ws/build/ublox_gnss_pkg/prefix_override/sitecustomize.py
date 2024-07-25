5409bb3a3e4f0280415bef805982f406938087fc
PVBrodersen <phbro19@student.sdu.dk>
2024-07-25 10:39:24 +0200

    Fixed

---
+++ ros2_ws/build/ublox_gnss_pkg/prefix_override/sitecustomize.py
@@ -0,0 +1,3 @@
+import sys
+sys.real_prefix = sys.prefix
+sys.prefix = sys.exec_prefix = '/home/philip/work/GNSS/Ublox_GNSS/ros2_ws/install/ublox_gnss_pkg'
---
+++ ros2_ws/install/ublox_gnss_pkg/lib/python3.10/site-packages/ublox_gnss_pkg/ublox_gnss_node.py
@@ -0,0 +1,54 @@
+import rclpy
+import serial
+from rclpy.node import Node
+from ublox_gps import UbloxGps
+from std_msgs.msg import String
+from sensor_msgs.msg import NavSatFix
+
+class GPSPublisher(Node):
+
+    def __init__(self):
+        super().__init__('gps_publisher')
+        self.publisher_ = self.create_publisher(NavSatFix, 'gps/fix', 10)
+        self.port = serial.Serial('/dev/serial0', baudrate=38400, timeout=1)
+        self.gps = UbloxGps(self.port)
+
+    def publish(self):
+        try: 
+            print("Listening for UBX Messages")
+            while True:
+                try:
+                    geo = self.gps.geo_coords()
+                    cov = self.gps.geo_cov()
+                    msg = NavSatFix()
+                    msg.longitude = geo.lon
+                    msg.latitude = geo.lat
+                    msg.altitude = 0.001 * float(geo.height)
+                    NN = cov.posCovNN
+                    NE = cov.posCovNE
+                    ND = cov.posCovND
+                    EE = cov.posCovEE
+                    ED = cov.posCovED
+                    DD = cov.posCovDD
+                    # Following the convention here: https://www.ros.org/reps/rep-0105.html
+                    msg.position_covariance = [EE, NE, ED, NE, NN, ND, ED, ND, DD]
+                    msg.position_covariance_type = 3
+                    self.publisher_.publish(msg)
+                except (ValueError, IOError) as err:
+                    print(err)
+        finally:
+            self.port.close()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    gps_publisher = GPSPublisher()
+    gps_publisher.publish()
+
+    gps_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
